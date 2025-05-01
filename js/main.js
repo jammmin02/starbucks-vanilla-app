a82a2e430764663fff1cae6c820f55665fa6a0fe
jammmin02 <jmkm1201@naver.com>
2025-05-01 17:28:30 +0900

    header:sub_menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector('.search'); // 검색창
+const searchInputEl = searchEl.querySelector('input'); // 검색창 input
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus(); // 검색창 클릭 시 input에 포커스
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
