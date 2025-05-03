388ba782679e4371224709b53b3551a53ef53523
jammmin02 <jmkm1201@naver.com>
2025-05-03 18:29:05 +0900

    add : season-product

--- js/youtube.js
+++ js/youtube.js
@@ -1,26 +1,25 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+// 2. 이 코드는 IFrame Player API 코드를 비동기로 로드함
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-function onYouTubeIframeAPIReady() { // 함수이름 변경 불가
+// 3. API 코드 다운로드 후 호출되는 함수 (YouTube 플레이어 생성)
+// ※ 함수 이름은 YouTube API가 자동으로 호출하기 때문에 변경하면 안 됨
+function onYouTubePlayerAPIReady() {
   // <div id="player"></div>
   new YT.Player('player', {
-    videoId: 'An6LvWQuj_8', // 최초 재생 할 유튜브 영상 ID
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
     playerVars: {
       autoplay: true, // 자동 재생 유무
       loop: true, // 반복 재생 유무
-      platList: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
-      onReady: function(event) {
-        event.targer.mute() // 음소거
+      // 영상이 준비되었을 때,
+      onReady: function (event) {
+        event.target.mute() // 음소거!
       }
     }
-    
-  });
+  })
 }
